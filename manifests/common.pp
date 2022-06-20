916e70b79ae154dd4fbe87e94b05b6a47e8500dc
Israel Calvete <icalvete@gmail.com>
2022-06-20 12:00:51 +0000

    update modules

--- manifests/common.pp
+++ manifests/common.pp
@@ -25,7 +25,7 @@ class php8::common inherits php8::params {
       case $version {
         0: {
           $extension_dir     = '/usr/lib/php/20200930'
-          $php8_modules = ["php8.${version}-curl","php8.${version}-mysql", "php8.${version}-mcrypt", "php8.${version}-gd", "php8.${version}-mbstring", "php8.${version}-bcmath", "php8.${version}-xml", "php8.${version}-sqlite3", "php8.${version}-zip", "php8.${version}-gmp", "php8.${version}-bz2",  'php-mongodb', "php8.${version}-memcached", 'php-imagick', 'php-redis']
+          $php8_modules = ["php8.${version}-curl","php8.${version}-mysql", "php8.${version}-mcrypt", "php8.${version}-gd", "php8.${version}-mbstring", "php8.${version}-bcmath", "php8.${version}-xml", "php8.${version}-sqlite3", "php8.${version}-zip", "php8.${version}-gmp", "php8.${version}-bz2",  "php8.${version}-mongodb", "php8.${version}-memcached", "php8.${vserion}-imagick", "php8.${version}-redis"]
         }
       }
     }
